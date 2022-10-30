180f65c68fa412ad9e49869c11be3a0d18d1f9d9
DESKTOP-GV962H6\sinke <sinkevich.work@gmail.com>
2022-10-30 20:33:36 +0300

    complected task

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -1,6 +1,25 @@
 package lang.print.gaps.task4;
 
 public class IntRepresentation {
+    //'c', 'h', 'a', 'r'
     public static void main(String[] args) {
+        int numA = 97;
+        char a = (char)numA;
+
+        int numH = 104;
+        char h = (char)numH;
+
+        int numR = 114;
+        char r = (char)numR;
+
+
+        int numC = 99;
+        char c = (char)numC;
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
+
+
     }
 }
